ffa5c57ccf07bae181d4712bb789a448df8c2c68
Lohit <t.lohit29@gmail.com>
2025-08-26 01:19:45 +0530

    Update arithmeticcalc.java    for jenkins

--- src/main/java/com/klu/controller/arithmeticcalc.java
+++ src/main/java/com/klu/controller/arithmeticcalc.java
@@ -10,17 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @CrossOrigin("*")
 public class arithmeticcalc {
-	@GetMapping("add/{A}/{B}")
-	public String add(@PathVariable("A") int a,@PathVariable("B") int b) {
-		return ("Addition =" + (a+b));
-	@GetMapping("sub/{A}/{B}")
-	public String add(@PathVariable("A") int a,@PathVariable("B") int b) {
-		return ("Subtraction =" + (a+b));
-	@GetMapping("div/{A}/{B}")
-	public String add(@PathVariable("A") int a,@PathVariable("B") int b) {
-		return ("division =" + (a+b));
-	@GetMapping("mul/{A}/{B}")
-	public String add(@PathVariable("A") int a,@PathVariable("B") int b) {
-		return ("multiplication =" + (a+b));
-	}
+
+    @GetMapping("add/{A}/{B}")
+    public String add(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Addition = " + (a + b);
+    }
+
+    @GetMapping("sub/{A}/{B}")
+    public String sub(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Subtraction = " + (a - b);
+    }
+
+    @GetMapping("mul/{A}/{B}")
+    public String mul(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Multiplication = " + (a * b);
+    }
+
+    @GetMapping("div/{A}/{B}")
+    public String div(@PathVariable("A") int a, @PathVariable("B") int b) {
+        if (b == 0) {
+            return "Error: Division by zero is not allowed!";
+        }
+        double result = (double) a / b;
+        return "Division = " + result;
+    }
 }
